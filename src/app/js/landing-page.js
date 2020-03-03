b045dfb56ba13a7effc383445721807282d932b1
Abidán Brito <abidan.brito@gmail.com>
2020-03-03 19:56:05 +0100

    Changed navigation bar completely

---
+++ src/app/js/landing-page.js
@@ -0,0 +1,25 @@
+const navSlider = () => {
+    const burgerIcon = document.querySelector('.menu-icon');
+    const navBar = document.querySelector('.navbar-links');
+    const navBarLinks = document.querySelectorAll('.navbar-links li');
+
+    burgerIcon.addEventListener('click', () => {
+        // Toggle navigation bar
+        navBar.classList.toggle('navbar-active');
+        
+        // Links fade in animation
+        navBarLinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            }
+            else {
+                link.style.animation = `navbarLinkFade 0.5s ease forwards ${index / 4 + 0.1}s`;
+            }
+        });
+
+        // Burger icon swap
+        burgerIcon.classList.toggle('menu-toggle');
+    });
+}
+
+navSlider();
