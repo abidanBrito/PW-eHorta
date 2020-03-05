3cf222005e0a73fe5afce35c8cf1775aae18ba9a
Abidán Brito <abidan.brito@gmail.com>
2020-03-05 19:51:04 +0100

    Navbar almost finished

--- src/app/js/landing-page.js
+++ src/app/js/landing-page.js
@@ -1,11 +1,15 @@
 const navSlider = () => {
     const burgerIcon = document.querySelector('.menu-icon');
     const navBar = document.querySelector('.navbar-links');
+    const loginIcon = document.querySelector('#login-icon');
     const navBarLinks = document.querySelectorAll('.navbar-links li');
 
     burgerIcon.addEventListener('click', () => {
         // Toggle navigation bar
         navBar.classList.toggle('navbar-active');
+                
+        // Hide login icon
+        loginIcon.classList.toggle('login-hide');
                 
         // Links fade in animation
         navBarLinks.forEach((link, index) => {
