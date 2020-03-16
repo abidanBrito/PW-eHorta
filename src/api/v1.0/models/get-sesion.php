d8221c59a2195b1f9e583cdcb13addc7ad6b0904
Abidán Brito <abidan.brito@gmail.com>
2020-03-16 13:36:03 +0100

    get-sesion minor fix

--- src/api/v1.0/models/get-sesion.php
+++ src/api/v1.0/models/get-sesion.php
@@ -1,4 +1,10 @@
 <?php
 require_once 'acceso.php';
-    session_start();
-    $http_code=200;
+
+session_start();
+if (isset($_SESSION['registered']) && $_SESSION['registered'] == 'ok') {
+    $http_code = 200;
+}
+else {
+    $http_code = 401;
+}
--- src/api/v1.0/models/post-sesion.php
+++ src/api/v1.0/models/post-sesion.php
@@ -1,12 +1,11 @@
 <?php
 
-    if((isset($_POST['pass']) && $_POST['pass'] !== '' )&&(isset($_POST['user']) && $_POST['user'] !== '' )){
-       
-        $user = $_POST['user'];
-        $key = $_POST['pass'];
+if((isset($_POST['pass']) && $_POST['pass'] !== '') && (isset($_POST['user']) && $_POST['user'] !== '')) {      
+    $user = $_POST['user'];
+    $key = $_POST['pass'];
         
-        //$sql = "SELECT * FROM `users` WHERE email = $user AND  password = $key";
-        $sql = "SELECT
+    //$sql = "SELECT * FROM `users` WHERE email = $user AND  password = $key";
+    $sql = "SELECT
     users.id,
     users.email,
     users.name,
@@ -18,30 +17,27 @@
         WHERE
     users.email = '$user' AND users.roleId = roles.id AND users.password = '$key'";
 
-        $res = mysqli_query($conn, $sql);
+    $res = mysqli_query($conn, $sql);
+    $array_sql = mysqli_fetch_assoc($res);
+    //var_dump($array_sql);
 
-        $array_sql = mysqli_fetch_assoc($res);
-       // var_dump($array_sql);
-
-       
-        if(mysqli_num_rows($res)>0){
-            session_start();
+    if(mysqli_num_rows($res) > 0) {
+        session_start();
             
-            $_SESSION['registrado'] = "sesion_registrada";
-            $_SESSION["user_id"]= $array_sql["id"];
-           // var_dump($_SESSION["user_id"]);
-
-            $_SESSION["user_email"]=$array_sql["email"];
-            $_SESSION["user_name"]=$array_sql["name"];
-            $_SESSION["user_roleId"]=$array_sql["roleId"];
-
+        $_SESSION['registrado'] = "sesion_registrada";
+        $_SESSION["user_id"]= $array_sql["id"];
+        $_SESSION["user_email"]=$array_sql["email"];
+        $_SESSION["user_name"]=$array_sql["name"];
+        $_SESSION["user_roleId"]=$array_sql["roleId"];
+        //var_dump($_SESSION["user_id"]);
         //var_dump( $_SESSION["user_email"]);
-
         
-            $http_code = 200;   //Todo okey            
-        }else{
-            $http_code = 401;   // Unauthorized
-        }
-    }else{
-        $http_code = 400; //Petición mal formulada
+        $http_code = 200;   // All good            
     }
+    else {
+        $http_code = 401;   // Unauthorized
+    }
+}
+else {
+    $http_code = 400; // Poorly formulated request
+}
