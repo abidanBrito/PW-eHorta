f24e99350b5e7d2c7762175d28f6e97202372ad4
Abidán Brito <abidan.brito@gmail.com>
2020-03-16 16:57:52 +0100

    Undo get sesion conditional

--- src/api/v1.0/models/get-sesion.php
+++ src/api/v1.0/models/get-sesion.php
@@ -1,10 +1,4 @@
 <?php
 require_once 'acceso.php';
-
 session_start();
-if (isset($_SESSION['registered']) && $_SESSION['registered'] == 'ok') {
-    $http_code = 200;
-}
-else {
-    $http_code = 401;
-}
+$http_code = 200;
