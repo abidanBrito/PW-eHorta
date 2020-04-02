75a40ad3b9ab46a225b6662a1f1c5ae35c47c6f0
Abidán Brito <abidan.brito@gmail.com>
2020-04-02 20:55:02 +0100

    Changed defaults for the map and added draw polygons functionality

--- src/app/js/map.js
+++ src/app/js/map.js
@@ -1,26 +1,45 @@
+/* ----------------------------------------------------------------
+*   AUTHOR:         Abidan Brito Clavijo, Luis Belloch Martinez
+*   FILE:           map.js
+*   DATE:           25/03/2020
+*   STATE:          WIP
+*   ----------------------------------------------------------------
+*   NOTICE: Copyright (c) 2020 Abidan Brito Clavijo
+*   ---------------------------------------------------------------- */
+
 function initMap() {
-    // The location of the initial zone in the map
-    let options = {
-        zoom: 0,
-        center: { lat: 39.497875, lng: -0.441465 },
-        mapTypeId: 'hybrid',
-        styles: [{
-                featureType: 'poi',
-                stylers: [{ visibility: "off" }]
-            },
-            {
-                featureType: 'transit',
-                stylers: [{ visibility: "off" }]
-            }
-        ]
+    // Initial map settings
+	let options = {
+		center: { lat: 36.052448, lng: -1.7467878 }, // Map center coordinates
+		zoom: 4,
+		mapTypeId: 'satellite',
+		styles: [
+			// Hide museums, monuments, etc.
+			{
+				featureType: 'poi',
+				stylers: [ { visibility: 'off' } ]
+			},
+
+			// Hide stations, bus stops, etc.
+			{
+				featureType: 'transit',
+				stylers: [ { visibility: 'off' } ]
+			}
+		],
+		mapTypeControl: false,
+		streetViewControl: false
     };
+    
     // The map, centered at options
     let map = new google.maps.Map(document.getElementById('map'), options);
+    
     // The markers, positioned at options
     addMarkers(map);
-    // The polygons, around the markers
-    addPaths(map);
-    // Refresca la gráfica
+
+	// The polygons, around the markers
+	addPaths(map);
+
+    // Refresh/update graph
     refreshGraphScript();
 }
 
@@ -34,53 +53,83 @@ function centerPlot(selectedPlot) {
             if(plot.id == selectedPlot){
                 rightPlot = plot;
             }
-        })
+        });
         let lati = parseFloat(rightPlot.latitude);
         let long = parseFloat(rightPlot.longitude);
         console.log(lati + "," + long)
-        // Modificamos el mapa para centrarlo en la parcela
+
+		// New map settings (centered around the field)
         let options = {
             zoom: 18,
             center: { lat: lati, lng: long },
-            mapTypeId: 'hybrid',
-            styles: [{
-                featureType: 'poi',
-                stylers: [{ visibility: "off" }]
-            },
-            {
-                featureType: 'transit',
-                stylers: [{ visibility: "off" }]
-            }
+            mapTypeId: 'satellite',
+            styles: [
+                // Hide museums, monuments, etc.
+                {
+                    featureType: 'poi',
+                    stylers: [{ visibility: "off" }]
+                },
+
+                // Hide stations, bus stops, etc.
+                {
+                    featureType: 'transit',
+                    stylers: [{ visibility: "off" }]
+                }
             ]
         };
-        // The map, centered at options
+
+        // Apply new settings to the map
         let map = new google.maps.Map(document.getElementById('map'), options);
-        // Show probes markers
+
+		// Draw probes markers
         showPositions(rightPlot,map);
-        // The polygons, around the markers
-        addPaths(map);
-    })
+
+		// Draw field polygons
+		drawTerrain(rightPlot.id, map);
+    });
 }
 
-function addPaths(map) {
-    fetch('../api/v1.0/positions').then(function(positions) {
-        return positions.json()
-    }).then(function(pos) {
-        let newPos = [];
-        for (let i = 0; i < pos.length; i++) {
-            newPos[i] = new google.maps.LatLng(pos[i].longitude, pos[i].latitude);
-        }
-        let polygon = new google.maps.Polygon({
-            paths: newPos,
-            strokeColor: "#ff8000",
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: "#ff8000",
-            fillOpacity: 0.35,
-            map: map
-        });
-        polygon.setMap(map);
-    })
+function drawTerrain(selectedPlotID, map) {
+	// Log trace
+	console.log(selectedPlotID);
+
+	fetch('../api/v1.0/polygons')
+		.then(function(response) {
+			// Error checking
+			if (response.ok) {
+				return response.json();
+			} else {
+				alert('Something went wrong fetching!');
+			}
+		})
+		.then(function(verticesJSON) {
+			// Empty array to store terrain bounds (vertices)
+			let terrainPath = [];
+
+			verticesJSON.forEach((vertex) => {
+				if (vertex.plot == selectedPlotID) {
+					let lat = parseFloat(vertex.latitude);
+					let lng = parseFloat(vertex.longitude);
+
+					let currVertex = new google.maps.LatLng(lat, lng);
+
+					terrainPath.push(currVertex);
+				}
+			});
+			console.log(terrainPath);
+
+			let polygon = new google.maps.Polygon({
+				paths: terrainPath,
+				strokeColor: '#ff8000',
+				strokeOpacity: 0.8,
+				strokeWeight: 2.5,
+				fillColor: '#ff8000',
+				fillOpacity: 0.35,
+				map: map
+			});
+
+			polygon.setMap(map);
+		});
 }
 
 function addMarkers(map) {
