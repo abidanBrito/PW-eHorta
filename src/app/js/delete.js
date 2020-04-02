0271723615a347477b5fc2a078d2c3f380c5929a
Luis Belloch <luis.bellochmartinez@gmail.com>
2020-04-02 19:41:25 +0200

    delete-plots-fix
    Now can delete plots from desktop and mobile versions

--- src/app/js/delete.js
+++ src/app/js/delete.js
@@ -13,18 +13,23 @@ function borrar(i) {
 }
 
 function borrarVarios() {
-    let input = document.getElementsByTagName('input');
     let conf = confirm("¿Está seguro que quiere borrar las parcelas seleccionadas? Esta acción no se puede revertir");
     if (conf == true) {
-        for (let i = 0; i < 10000; i++) {
-            if (input[i].getAttribute('type') == "checkbox") {
-                if (input[i].checked == true) {
-                    borrar(input[i].value)
+        if (document.getElementById("select-css").style.display != "none") {
+            console.log("ENTRA" + document.getElementById("select-css").value)
+            borrar(document.getElementById("select-css").value)
+        } else {
+            let input = document.getElementsByTagName('input');
+            for (let i = 0; i < input.length; i++) {
+                if (input[i].getAttribute('type') == "checkbox") {
+                    if (input[i].checked == true) {
+                        borrar(input[i].value)
+                    }
                 }
             }
         }
-    }
-    if (conf == false) {
-        location.href = 'app.html';
+        if (conf == false) {
+            location.href = 'app.html';
+        }
     }
 }
