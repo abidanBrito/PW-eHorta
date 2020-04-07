ed446f3f65e417913e1aa3d06da6cb2c9f92058f
Jorge Grau <jgragia@epsg.upv.es>
2020-04-07 19:29:24 +0200

    Fixed login (role_id)

--- src/api/v1.0/models/post-sesion.php
+++ src/api/v1.0/models/post-sesion.php
@@ -5,33 +5,22 @@ if((isset($_POST['pass']) && $_POST['pass'] !== '') && (isset($_POST['user']) &&
     $key = $_POST['pass'];
         
     //$sql = "SELECT * FROM `users` WHERE email = $user AND  password = $key";
-    $sql = "SELECT
-    users.id,
-    users.email,
-    users.name,
-    users.roleId,
-    roles.name
-        FROM
-    users,
-    roles
-        WHERE
-    users.email = '$user' AND users.roleId = roles.id AND users.password = '$key'";
+    $sql = "SELECT users.id, users.email, users.name, users.roleId, roles.name FROM users, roles WHERE users.email = '$user' AND users.roleId = roles.id AND users.password = '$key'";
+    
+    
 
     $res = mysqli_query($conn, $sql);
-    $array_sql = mysqli_fetch_assoc($res);
-    //var_dump($array_sql);
-
     if(mysqli_num_rows($res) > 0) {
+        $array_sql = mysqli_fetch_assoc($res);
+
         session_start();
-            
         $_SESSION['registrado'] = "sesion_registrada";
         $_SESSION["user_id"]= $array_sql["id"];
         $_SESSION["user_email"]=$array_sql["email"];
         $_SESSION["user_name"]=$array_sql["name"];
         $_SESSION["user_roleId"]=$array_sql["roleId"];
-        //var_dump($_SESSION["user_id"]);
-        //var_dump( $_SESSION["user_email"]);
         
+        array_push($salida, $array_sql["roleId"]);
         $http_code = 200;   // All good            
     }
     else {
--- src/app/js/sesion.js
+++ src/app/js/sesion.js
@@ -4,12 +4,27 @@ function enviar(evento) {
         method: 'post',
         body: new FormData(document.getElementById('formKey')),
         credentials: "same-origin"
-    }).then(function(respuesta) {
-        if (respuesta.status == 200) {
-            location.href = 'app.html';
-        } else {
+        
+    }).then(function (answer) {
+        if (answer.status == 200){
+            return answer.json();
+        } 
+        else{
             document.getElementById("error_msg").style.visibility = "visible";
-            document.getElementById("error_msg").style.display = "block";
-        }
-    })
+        } 
+    }).then(function(jsonData) {
+        
+            if( jsonData[0] == "1" ){
+                location.href = 'app.html';
+            }
+            if( jsonData[0] == "2" ){
+                location.href = 'admin.html';
+            }
+            if( jsonData[0] == "3" ){
+                location.href = 'app.html';
+            }
+            if( jsonData[0] == "4" ){
+                location.href = 'app.html';
+            }
+    });
 }
