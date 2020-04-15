041866f5974a7e09377de55fee6992690add56b3
Abidán Brito <abidan.brito@gmail.com>
2020-04-15 10:19:24 +0100

    feature: all polygons get drawn at once (selected polygon in blue)

--- src/api/v1.0/models/get-polygons.php
+++ src/api/v1.0/models/get-polygons.php
@@ -2,7 +2,7 @@
 /* ----------------------------------------------------------------
 *   AUTHOR:         Abidan Brito Clavijo
 *   FILE:           get-polygons.php
-*   DATE:           25/03/2020
+*   DATE:           08/04/2020
 *   STATE:          DONE
 *   ----------------------------------------------------------------
 *   NOTICE: Copyright (c) 2020 Abidan Brito Clavijo
@@ -14,14 +14,15 @@ require_once 'acceso.php';
 $user = $_SESSION["user_id"];
 
 // Define SQL query
-$sql = "SELECT `vertex`.`id`, `vertex`.`plot`, `vertex`.`latitude`, `vertex`.`longitude` 
-FROM `vertex`"; 
+$sql = "SELECT p.*, CONCAT('[', GROUP_CONCAT(JSON_OBJECT('lat', v.latitude, 'lng', v.longitude)), ']') 
+AS plotPath FROM `plots` AS p INNER JOIN `vertex` AS v ON p.id = v.plot GROUP BY p.id";
 
 // Send SQL query to the active database
 $res = mysqli_query($conn, $sql);
 
-while($row = mysqli_fetch_assoc($res)) {
+while ($row = mysqli_fetch_assoc($res)) {
     array_push($salida, $row);
 }
-    
+
 $http_code = 200;
+?>
--- src/app/js/map.js
+++ src/app/js/map.js
@@ -7,50 +7,51 @@
 
 function initMap() {
     // Initial map settings
-	let options = {
-		center: { lat: 36.052448, lng: -1.7467878 }, // Map center coordinates
-        zoom: 4,
+    let options = {
+        center: {lat: 28.493408, lng: -14.019035}, // Map center coordinates
+        zoom: 17,
         tilt: 0,
-		mapTypeId: 'satellite',
-		styles: [
-			// Hide museums, monuments, etc.
-			{
-				featureType: 'poi',
-				stylers: [ { visibility: 'off' } ]
-			},
+        mapTypeId: 'satellite',
+        styles: [
+            // Hide museums, monuments, etc.
+            {
+                featureType: 'poi',
+                stylers: [{visibility: 'off'}]
+            },
 
-			// Hide stations, bus stops, etc.
-			{
-				featureType: 'transit',
-				stylers: [ { visibility: 'off' } ]
-			}
+            // Hide stations, bus stops, etc.
+            {
+                featureType: 'transit',
+                stylers: [{visibility: 'off'}]
+            }
         ],
         rotateControl: false,
-		mapTypeControl: false,
-		streetViewControl: false
+        mapTypeControl: false,
+        streetViewControl: false
     };
-    
+
     // The map, centered at options
     let map = new google.maps.Map(document.getElementById('map'), options);
-    
-    // The markers, positioned at options
-    addMarkers(map);
 
-	// The polygons, around the markers
-	drawTerrain(map);
+    // The polygons, around the markers
+    drawTerrain(map);
 
     // Refresh/update graph
     refreshGraphScript();
 }
 
+/**
+ *
+ * @param selectedPlot
+ */
 function centerPlot(selectedPlot) {
-    fetch('../api/v1.0/plots').then(function(plots) {
+    fetch('../api/v1.0/plots').then(function (plots) {
         return plots.json();
-    }).then(function(plotsJson) {
+    }).then(function (plotsJson) {
         let rightPlot = {};
         data.forEach((plot) => {
             // Comparamos el id recibido de la tabla con el de cada parcela recibida del fetch y lo devolvemos a la siguiente funcion
-            if(plot.id == selectedPlot){
+            if (plot.id == selectedPlot) {
                 rightPlot = plot;
             }
         });
@@ -58,23 +59,23 @@ function centerPlot(selectedPlot) {
         let long = parseFloat(rightPlot.longitude);
         console.log(lati + "," + long)
 
-		// New map settings (centered around the field)
+        // New map settings (centered around the field)
         let options = {
             zoom: 18,
             tilt: 0,
-            center: { lat: lati, lng: long },
+            center: {lat: lati, lng: long},
             mapTypeId: 'satellite',
             styles: [
                 // Hide museums, monuments, etc.
                 {
                     featureType: 'poi',
-                    stylers: [{ visibility: "off" }]
+                    stylers: [{visibility: "off"}]
                 },
 
                 // Hide stations, bus stops, etc.
                 {
                     featureType: 'transit',
-                    stylers: [{ visibility: "off" }]
+                    stylers: [{visibility: "off"}]
                 }
             ],
             rotateControl: false,
@@ -82,65 +83,73 @@ function centerPlot(selectedPlot) {
             streetViewControl: false
         };
 
+
         // Apply new settings to the map
         let map = new google.maps.Map(document.getElementById('map'), options);
 
-		// Draw probes markers
-        showPositions(rightPlot,map);
+        // Draw probes markers
+        showPositions(rightPlot, map);
 
-		// Draw field polygons
-		drawTerrain(rightPlot.id, map);
+        // Draw field polygons
+        drawTerrain(map, rightPlot.id);
     });
 }
 
-function drawTerrain(selectedPlotID, map) {
-	// Log trace
-	console.log(selectedPlotID);
+/**
+ *
+ * @param map
+ * @param selectedPlotID
+ */
+function drawTerrain(map, selectedPlotID = null) {
+    // Log trace
+    console.log(selectedPlotID);
 
-	fetch('../api/v1.0/polygons')
-		.then(function(response) {
-			// Error checking
-			if (response.ok) {
-				return response.json();
-			} else {
-				alert('Something went wrong fetching!');
-			}
-		})
-		.then(function(verticesJSON) {
-			// Empty array to store terrain bounds (vertices)
-			let terrainPath = [];
+    fetch('../api/v1.0/polygons')
+        .then(function (response) {
+            // Error checking
+            if (response.ok) {
+                return response.json();
+            }
+            else {
+                alert('Something went wrong fetching!');
+            }
+        })
+        .then(function (plotJson) {
+            let bounds = new google.maps.LatLngBounds();
+            plotJson.forEach((plot) => {
+                let polygon = new google.maps.Polygon({
+                    paths: JSON.parse(plot.plotPath),
+                    strokeColor: '#ff8000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2.5,
+                    fillColor: '#ff8000',
+                    fillOpacity: 0.35,
+                    map: map
+                });
 
-			verticesJSON.forEach((vertex) => {
-				if (vertex.plot == selectedPlotID) {
-					let lat = parseFloat(vertex.latitude);
-					let lng = parseFloat(vertex.longitude);
+                if(selectedPlotID !== null) {
 
-					let currVertex = new google.maps.LatLng(lat, lng);
+                    if (plot.id == selectedPlotID) {
+                        polygon.strokeColor = '#00a5ff';
+                        polygon.fillColor = '#00a5ff';
+                    };
+                }
 
-					terrainPath.push(currVertex);
-				}
-			});
-			console.log(terrainPath);
-
-			let polygon = new google.maps.Polygon({
-				paths: terrainPath,
-				strokeColor: '#ff8000',
-				strokeOpacity: 0.8,
-				strokeWeight: 2.5,
-				fillColor: '#ff8000',
-				fillOpacity: 0.35,
-				map: map
-			});
-
-			polygon.setMap(map);
-		});
+                polygon.getPath().getArray().forEach(function (vertex) {
+                    bounds.extend(vertex);
+                });
+            });
+        });
 }
 
+/*
 function addMarkers(map) {
-    fetch('../api/v1.0/plots').then(function(j) { return j.json() }).then(function(pos) {
+    fetch('../api/v1.0/plots').then(function (j) {
+        return j.json()
+    }).then(function (pos) {
         pos.forEach((plot) => {
             let newPos = new google.maps.LatLng(`${plot.latitude}`, `${plot.longitude}`);
-            
+
             let marker = new google.maps.Marker({
                 position: newPos,
                 map: map,
@@ -150,46 +159,47 @@ function addMarkers(map) {
         })
     });
 }
+*/
 
-function showPositions(selectedPlot, map) { 
-    // Recoge todas las posiciones de la base de datos 
-    fetch('../api/v1.0/positions').then(function(data) { 
-        return data.json(); 
-    }).then(function(dataJson) { 
-        let plotPositions = []; 
-         
-        // Guarda en un array las posiciones de la parcela seleccionada y los muestra como marcadores 
+function showPositions(selectedPlot, map) {
+    // Recoge todas las posiciones de la base de datos
+    fetch('../api/v1.0/positions').then(function (data) {
+        return data.json();
+    }).then(function (dataJson) {
+        let plotPositions = [];
+
+        // Guarda en un array las posiciones de la parcela seleccionada y los muestra como marcadores
         dataJson.forEach((pos) => {
-            if(`${pos.plot}` == selectedPlot.id) { 
-                plotPositions.push(pos); 
-                // Se guarda la posición 
-                let newPos = new google.maps.LatLng(`${pos.latitude}`, `${pos.longitude}`) 
-                // Se crean los marcadores 
-                let marker = new google.maps.Marker({ 
-                    position: newPos, 
-                    map: map, 
+            if (`${pos.plot}` == selectedPlot.id) {
+                plotPositions.push(pos);
+                // Se guarda la posición
+                let newPos = new google.maps.LatLng(`${pos.latitude}`, `${pos.longitude}`)
+                // Se crean los marcadores
+                let marker = new google.maps.Marker({
+                    position: newPos,
+                    map: map,
                     animation: google.maps.Animation.DROP,
                     title: `${pos.id}`
                 });
-                marker.addListener('click', function() {
+
+                marker.addListener('click', function () {
                     // Pone el marcador en marcado
                     document.getElementById('selectedProbe').innerHTML = this.title;
                     // Refresca la gráfica
                     refreshGraphScript();
                 });
-            } 
-        }) 
-         
-        console.log(plotPositions); 
-    }) 
+            }
+        })
+
+        console.log(plotPositions);
+    })
 }
 
-function refreshGraphScript(){
-    
+function refreshGraphScript() {
     let script = document.createElement("script");
     script.src = 'js/graphic-data.js';
     console.log("Script created");
-    
+
     document.head.appendChild(script);
     script.parentNode.removeChild(script);
 }
