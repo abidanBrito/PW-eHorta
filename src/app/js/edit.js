740e5a7781a4d07a895364a87565b9f8bbb2ffd4
Abidán Brito <abidan.brito@gmail.com>
2020-04-21 20:15:27 +0100

    chore: add and edit user/fields

---
+++ src/app/js/edit.js
@@ -0,0 +1,20 @@
+/* ----------------------------------------------------------------
+*   AUTHOR:         Jorge Grau
+*   FILE:           edit.js
+*   DATE:           19/04/2020
+*   STATE:          DONE
+*  ---------------------------------------------------------------- */    
+function modify_plot(evento) {
+    evento.preventDefault();
+    fetch('../api/v1.0/edit-field', {
+        method: 'post',
+        credentials:"same-origin",
+        body: new FormData(document.getElementById('fieldModForm'))
+    }).then(function(respuesta) {
+        if (respuesta.status == 200) {
+            location.href="admin.html" //Pagina desde donde se ha clickado añadir
+        } else {
+            document.getElementById("error_msg").style.display = "block";
+        }
+    })
+}  
