af2aa7cdcc29b8df49ffd855177a86391d6e8cf3
Pablo Enguix <pabloenguixllopis@gmail.com>
2020-04-22 18:46:51 +0200

    fix: PART 2 IS HERE. Also changing edit.js names

---
+++ src/api/v1.0/models/post-delete_user.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'access.php';
+
+$id = $_POST['id'];
+$sql = "DELETE FROM users WHERE id=$id";
+
+if ($conn->query($sql) === TRUE) {
+    $http_code = 200;
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
