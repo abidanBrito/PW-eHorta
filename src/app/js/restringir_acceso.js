a1ee8406eb0dc4c512f394a71863a12a896f4e59
Abidán Brito <abidan.brito@gmail.com>
2020-04-24 02:27:35 +0100

    fix: no more page flashes for unathorized access

--- src/app/js/restringir_acceso.js
+++ src/app/js/restringir_acceso.js
@@ -2,4 +2,7 @@ fetch('../api/v1.0/sesion').then(function(res) {
     if (res.status != 200) {
         location.href = 'access_error.html';
     }
+    else {
+        document.body.style.display = "block";
+    }
 })
