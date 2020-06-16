4ea14c92b044d37862f5dd4c5bff4c601c1cf2d6
Luis Belloch <luis.bellochmartinez@gmail.com>
2020-06-16 12:28:03 +0200

    Localization index and weather
    Navbar button to change language added

---
+++ src/app/js/localization.js
@@ -0,0 +1,185 @@
+/* ----------------------------------------------------------------
+*   AUTHOR:         Luis Belloch
+*   FILE:           localization.js
+*   DATE:           15/06/2020
+*   STATE:          WIP
+*  ---------------------------------------------------------------- */
+
+var arrLang = {
+    'en' : {
+        // El Tiempo
+        weather : 'Weather',
+        alerts : 'Alerts',
+        thresholds : 'Thresholds',
+        graph : 'Graphs',
+        aemet : 'Information elaborated by AEMET',
+        close_session : 'Log Out',
+        today : 'Today',
+        tomorrow : 'Tomorrow',
+        after_tomorrow : 'After Tomorrow',
+        prob : 'prob',
+        wind : 'wind',
+        humidty : 'humidity',
+        // Landing page index
+        home : "Home",
+        about : "About us",
+        faq : "FAQs",
+        contact : "Contact",
+        login : "Login",
+        slogan : "Agriculture from the future has arrived",
+        costs : "Reduce costs and production time, increase harvest.",
+        know_us : "Get to know us",
+        offer : "What we offer",
+        capacities : "Discover what our app can do",
+        multifunction : "Multifunction",
+        multifunction_txt : "Graphs, alerts, thresholds, geolocalized drones and much more",
+        fast : "Fast",
+        fast_txt : "Consult real-time data anytime",
+        multiplatform : "Multiplatform",
+        multiplatform_txt : "Access our app from any device",
+        contact_us : "Contact us now",
+        measure : "Available data",
+        measure_txt : "We get data from this magnitudes",
+        temperature : "Temperature",
+        luminosity : "Luminosity",
+        salinity : "Salinity",
+        humidity : "Humidity",
+        preassure : "Preassure",
+        precipitation : "Precipitation",
+        gps : "GPS",
+        anti_theft : "Anti-theft",
+        reviews_title : "Reviews",
+        reviews_txt : "Discover our users opinions",
+        review_1 : '"It reduces work hours, optimizing farming techniques."',
+        review_2 : '"A wise choice! In a year I have improved production a 15%"',
+        review_3 : '"Now I can manage my fields from anywhere with e-Horta."'
+        
+    },
+    'es' : {
+        // El Tiempo
+        weather : 'El Tiempo',
+        alerts : 'Alertas',
+        thresholds : 'Umbrales',
+        graph : 'Gráficas',
+        aemet : 'Información elaborada por la Agencia Estatal de Meteorología',
+        close_session : 'Cerrar Sesión',
+        today : 'Hoy',
+        tomorrow : 'Mañana',
+        after_tomorrow : 'Pasado Mañana',
+        prob : 'prob',
+        wind : 'viento',
+        humidty : 'humedad',
+        // Landing page index
+        home : "Inicio",
+        about : "Nosotros",
+        faq : "FAQs",
+        contact : "Contacto",
+        login : "Iniciar sesión",
+        slogan : "La agricultura del futuro ha llegado",
+        costs : "Reduce costes y tiempos de producción, aumenta la cosecha.",
+        know_us : "Conócenos",
+        offer : "Ofrecemos",
+        capacities : "Descubre las capacidades de nuestra aplicación",
+        multifunction : "Multifunción",
+        multifunction_txt : "Gráficas, alertas, umbrales, sondas geolocalizadas y mucho más.",
+        fast : "Rápido",
+        fast_txt : "Revisa los datos en tiempo real en cualquier momento.",
+        multiplatform : "Multiplataforma",
+        multiplatform_txt : "Accede a la aplicación desde cualquier plataforma.",
+        contact_us : "Ponte en contacto",
+        measure : "Medimos",
+        measure_txt : "Recopilamos datos de muchas magnitudes",
+        temperature : "Temperatura",
+        luminosity : "Luminosidad",
+        salinity : "Salinidad",
+        humidity : "Humedad",
+        preassure : "Presión",
+        precipitation : "Precipitación",
+        gps : "GPS",
+        anti_theft : "Antirrobo",
+        reviews_title : "Opiniones",
+        reviews_txt : "Descubre la opinión de nuestros usuarios",
+        review_1 : '"Ahorra muchas horas de trabajo, optimizando las técnicas de cultivo."',
+        review_2 : '"¡Todo un acierto! En un año ha mejorado la producción un 15%."',
+        review_3 : '"Ahora puedo gestionar mis tierras desde cualquier sitio con e-Horta."'
+    },
+    'val' : {
+        // El Tiempo
+        weather : "L'Oratge",
+        alerts : 'Alertes',
+        thresholds : 'Umbrals',
+        graph : 'Gràfiques',
+        aemet : "Informació elaborada per l'Agencia Estatal de Meteorología",
+        close_session : 'Tancar Sessió',
+        today : 'Hui',
+        tomorrow : 'Demà',
+        after_tomorrow : 'Despusdemà',
+        prob : 'prob',
+        wind : 'vent',
+        humidty : 'humitat',
+        // Landing page index
+        home : "Inici",
+        about : "Nosatros",
+        faq : "FAQs",
+        contact : "Contacte",
+        login : "Iniciar sessió",
+        slogan : "L'agricultura del futur ha aplegat",
+        costs : "Reduix costos y temps de producció, aumenta la collita.",
+        know_us : "Coneix-nos",
+        offer : "Oferim",
+        capacities : "Descobrix les capacitats de la nostra aplicació",
+        multifunction : "Multifunció",
+        multifunction_txt : "Gràfiques, alertes, umbrals, sondes geolocalisadess y molr més.",
+        fast : "Ràpit",
+        fast_txt : "Revisa les dates en temps real en qualsevol moment.",
+        multiplatform : "Multiplataforma",
+        multiplatform_txt : "Accedix a l'aplicació des de qualsevol plataforma.",
+        contact_us : "Posat en contacte",
+        measure : "Mesurem",
+        measure_txt : "Recopilem dates de moltes magnituds",
+        temperature : "Temperatura",
+        luminosity : "Lluminositat",
+        salinity : "Salinitat",
+        humidity : "Humitat",
+        preassure : "Pressió",
+        precipitation : "Precipitació",
+        gps : "GPS",
+        anti_theft : "Antirrobament",
+        reviews_title : "Opinions",
+        reviews_txt : "Descobrix les opinions dels nostres usuaris",
+        review_1 : '"Estalvia moltes hores de treball, optimisant les tècniques de cultiu."',
+        review_2 : '"Tot un encert! En un any ha millorat la producció un 15%."',
+        review_3 : '"Ara puc gestionar els meus camps des de qualsevol lloc en e-Horta."'
+        
+    }
+};
+
+function translate(btn) {
+    // lang will be its id, which is the language chosen
+    let lang = btn.id
+    // searches for all items with class="lang" and changes its text to the one associated within their "key" to the variable arrLang
+    let texts = document.getElementsByClassName('lang')
+    Array.prototype.forEach.call(texts, item => {
+        let translation = arrLang[lang]
+        item.innerHTML = translation[item.id]
+    })
+    // Cierra el tooltip
+    const tooltip = document.getElementById('lang-selector');
+    tooltip.style.display = "none";
+}
+
+function prepare() {
+    const button = document.getElementById('globe-icon');
+    const tooltip = document.getElementById('lang-selector');
+    tooltip.style.display = "none";
+    // Cuando se pulsa el icono del globo, se abre o cierra el tooltip
+    button.onclick = function() {
+        if(tooltip.style.display != "block") {
+            tooltip.style.display = "block";
+        } else {
+            tooltip.style.display = "none";
+        }
+    };
+}
+
+prepare();
